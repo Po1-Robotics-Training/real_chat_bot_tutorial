a9fdf59ac3267dcd10b20ab481d0a6015b2bbd9b
wqLouis <wongqilyo@gmail.com>
2025-06-12 17:10:36 +0800

    Change something

--- code/main.py
+++ code/main.py
@@ -6,93 +6,44 @@ import os
 from matplotlib import pyplot
 
 sd.default.samplerate = 8000
-sound_level_threshold = 0.1
+sound_level_threshold = 0.05
 audio_data = np.array([])
+inactive_count:float = 0.0
+inactive_for_time:float = 2.0
 
 lock = threading.Lock()
 
 #Recording
 def callback(indata, frames, time, status):
-    global audio_data
-    with lock:
-        audio_data = np.append(audio_data, indata[:,0])
+    split_chunk(indata=indata, frames=frames)
 def start_recording():
     with sd.InputStream(callback=callback, channels=1):
         while True:
             time.sleep(1/sd.default.samplerate)
 
-#split audio_data into chunks for data processing
-def split_chunk(is_active_from_previous_segment:bool):
-    global audio_data
-    with lock:
-        chunk_audio:np.array = audio_data
-    while len(chunk_audio) <= 0:
-        time.sleep(1/sd.default.samplerate)
+def split_chunk(indata, frames):
+    global inactive_count, audio_data
+    if np.mean(np.abs(indata)) > sound_level_threshold:
+        with lock:
+            inactive_count = 0
+            audio_data = np.append(audio_data, indata[:, 0])
+    else:
+        with lock:
+            inactive_count += frames/sd.default.samplerate
 
-    if is_active_from_previous_segment:
-        active_frame_start = 0
-
-    chunk_data:np.array = np.array([])
-
-    active_for_time:int = sd.default.samplerate * 1
-    inactive_for_time:int = sd.default.samplerate * 2
-
-    frame:int = 0
-
-    active_counter:int = 0
-    inactive_counter:int = 0
-
-    active_frame_start:int = None
-    active_frame_end:int = None
-    
-    is_active:bool = None
-
-    while True:
-        print('audio_data')
-        if frame >= len(chunk_audio) - 1:
-            if is_active and active_frame_start != None:
-                chunk_data = chunk_audio[active_frame_start:len(chunk_audio) - 1]
-            if active_frame_start != None and active_frame_end != None:
-                chunk_data = chunk_audio[active_frame_start:active_frame_end]
-
-            with lock:
-                audio_data = np.delete(arr=audio_data, obj=range(0,len(chunk_audio)))
-
-            return is_active, chunk_data
-        
-        if abs(chunk_audio[frame]) >= sound_level_threshold:
-            is_active = True
-            active_counter += 1
-            if active_frame_start == None:
-                active_frame_start = frame
-        else:
-            if is_active == False:
-                inactive_counter += 1
-            is_active = False
-            if active_frame_end == None and active_frame_start != None and inactive_counter > inactive_for_time:
-                active_frame_end = frame
-        
-        frame += 1
+    if inactive_count > inactive_for_time:
+        with lock:
+            audio_data = np.array([])
 
 #Main thread
 def Main():
-    chunk:np.array = np.array([])
-    is_active_from_previous_chunk:bool = False
-    current_chunk:np.array = np.array([])
+    global audio_data
     while True:
-        current_chunk = np.array([])
+        time.sleep(1/sd.default.samplerate)
         print(audio_data)
-        is_active_from_previous_chunk, current_chunk = split_chunk(is_active_from_previous_segment=is_active_from_previous_chunk)
-        print(audio_data)
-        chunk = np.append(chunk, current_chunk)
 
-        if is_active_from_previous_chunk == False:
-            print('Chunk fisished\n')
-            print(current_chunk)
+recording_thread = threading.Thread(target=start_recording)
+main_thread = threading.Thread(target=Main)
 
-if __name__ == '__main__':
-    recording_thread = threading.Thread(target=start_recording)
-    main_thread = threading.Thread(target=Main)
-
-    recording_thread.start()
-    main_thread.start()
+recording_thread.start()
+main_thread.start()
