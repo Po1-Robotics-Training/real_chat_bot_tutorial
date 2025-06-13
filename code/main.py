bef1aeb0942caa1fbe252b1e72bf22a701457a11
wqLouis <wongqiluo@gmail.com>
2025-06-13 16:26:55 +0800

    Refactor audio processing and enhance added stt whisper model

--- code/main.py
+++ code/main.py
@@ -2,18 +2,28 @@ import sounddevice as sd
 import threading
 import numpy as np
 import time
+import librosa
 import os
-from matplotlib import pyplot
+import glob
+from transformers import WhisperForConditionalGeneration, WhisperProcessor
 
-sd.default.samplerate = 8000
+sd.default.samplerate = 16000
 sound_level_threshold = 0.05
 audio_data = np.array([])
+audio_queue:list = []
 inactive_count:float = 0.0
 inactive_for_time:float = 2.0
 
+MODEL_NAME = "alvanlii/whisper-small-cantonese"
+print('modelname')
+processor = WhisperProcessor.from_pretrained(MODEL_NAME)
+print('processor')
+model = WhisperForConditionalGeneration.from_pretrained(MODEL_NAME)
+print('model')
+
 lock = threading.Lock()
 
-#Recording
+#Recording thread
 def callback(indata, frames, time, status):
     split_chunk(indata=indata, frames=frames)
 def start_recording():
@@ -22,7 +32,7 @@ def start_recording():
             time.sleep(1/sd.default.samplerate)
 
 def split_chunk(indata, frames):
-    global inactive_count, audio_data
+    global inactive_count, audio_data, audio_queue
     if np.mean(np.abs(indata)) > sound_level_threshold:
         with lock:
             inactive_count = 0
@@ -33,14 +43,33 @@ def split_chunk(indata, frames):
 
     if inactive_count > inactive_for_time:
         with lock:
+            if len(audio_data) > 0:
+                audio_queue.append(audio_data.tolist())
             audio_data = np.array([])
+
+def process_stt(indata:np.array):
+    processed_in = processor(indata, sampling_rate = sd.default.samplerate, return_tensors = "pt")
+    print('generate')
+    gout = model.generate(
+        input_features = processed_in.input_features,
+        output_scores = True,
+        return_dict_in_generate = True
+    )
+    transcription = processor.batch_decode(gout.sequences, skip_special_tokens=True)[0]
+    print(transcription)
 
 #Main thread
 def Main():
-    global audio_data
+    global audio_data, audio_queue
+    process_queue:np.array = np.array([])
+    with lock:
+        process_queue = audio_queue
     while True:
-        time.sleep(1/sd.default.samplerate)
-        print(audio_data)
+        while len(process_queue) > 0:
+            process_stt(process_queue[0])
+            with lock:
+                process_queue = audio_queue
+            process_queue = np.delete(process_queue, 0)
 
 recording_thread = threading.Thread(target=start_recording)
 main_thread = threading.Thread(target=Main)
